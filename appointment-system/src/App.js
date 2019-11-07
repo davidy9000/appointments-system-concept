160e991c0502285815fff45041b4a55c8688ec8a
davidy.9000 <davidy.9000@gmail.com>
2019-11-07 10:58:07 -0500

    it compiles

--- appointment-system/src/App.js
+++ appointment-system/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 import logo from "./logo.svg";
 import AppointmentApp from "./components/AppointmentApp.js";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
